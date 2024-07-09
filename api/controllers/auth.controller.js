1ee652c0cb441a77d1a3f43c695eff9d3571e6e7
AMOO <amoosamson@gmail.com>
2024-07-09 03:57:52 +0100

    new

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+
+
+
+
+
+
+export const register = async(req, res) => {
+
+
+    
+
+};
+export const login = async(req, res) => {
+
+};
+export const logout = (req, res) => {
+
+};
